0c200962a517d2db7d6158603c152758b54682e5
Quintin Kruger <qkruger@jhb.dvt.co.za>
2022-01-28 11:40:17 +0200

    Added rabbitmq retry connect functionality

--- security_config_subscriber.py
+++ security_config_subscriber.py
@@ -1,6 +1,8 @@
 import json
+import time
 
 import pika
+from pika.exceptions import AMQPConnectionError
 
 from service.alert_delegator_service import AlerterService
 import logging
@@ -14,6 +16,12 @@ class SecurityConfigSubscriber:
                  alerter_service: AlerterService):
         self.received_security_config = {}
         self.alerter_service = alerter_service
+        self.rabbitmq_host = rabbitmq_host
+        self.alerter_security_config_queue_name = alerter_security_config_queue_name
+        self.exchange_name = exchange_name
+        self.connect(alerter_security_config_queue_name, exchange_name, rabbitmq_host)
+
+    def connect(self, alerter_security_config_queue_name, exchange_name, rabbitmq_host):
         connection = pika.BlockingConnection(
             pika.ConnectionParameters(host=rabbitmq_host)
         )
@@ -21,19 +29,22 @@ class SecurityConfigSubscriber:
         self.channel.exchange_declare(exchange=exchange_name,
                                       exchange_type='fanout',
                                       durable=True)
-
         self.channel.queue_declare(queue=alerter_security_config_queue_name)
-
         self.channel.queue_bind(exchange=exchange_name,
                                 queue=alerter_security_config_queue_name)
-
         self.channel.basic_consume(queue=alerter_security_config_queue_name,
                                    on_message_callback=self.receive_security_config,
                                    auto_ack=True)
 
     def listen_for_security_config_messages(self):
-        logging.info("Listening for messages...")
-        self.channel.start_consuming()
+        try:
+            logging.info("Listening for messages...")
+            self.channel.start_consuming()
+        except AMQPConnectionError:
+            time.sleep(5)
+            logging.info("Connection failed, attempting to reconnect.")
+            self.connect(self.alerter_security_config_queue_name, self.exchange_name, self.rabbitmq_host)
+            self.listen_for_security_config_messages()
 
     def receive_security_config(self, ch, method, properties, body):
         security_config_string = body.decode()
