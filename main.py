f0a2c5dc7325cacac5bbeef1781bd91f36871376
Quintin Kruger <qkruger@jhb.dvt.co.za>
2022-01-28 08:10:08 +0200

    Added argument parser to obtain properties via cmdline

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from logging_setup import LoggingSetup
-import sys
+import argparse
 from threading import Thread
 
 from service.alert_delegator_service import AlerterService
@@ -9,13 +9,24 @@ from service.security_state_alerter_service import SecurityStateAlerterService
 from service.security_status_alerter_service import SecurityStatusAlerterService
 
 if __name__ == '__main__':
-    logging_file_location = sys.argv[1]
-    rabbitmq_parameters = sys.argv[2:]
-    rabbitmq_host = rabbitmq_parameters[0]
-    alerter_security_config_queue_name = rabbitmq_parameters[1]
-    exchange_name = rabbitmq_parameters[2]
-    logging_setup = LoggingSetup(logging_file_location)
+    argparse = argparse.ArgumentParser()
+    argparse.add_argument("--logging_file_location", required=True)
+    argparse.add_argument("--rabbitmq_host", required=True)
+    argparse.add_argument("--alerter_security_config_queue_name", required=True)
+    argparse.add_argument("--exchange_name", required=True)
+    arguments = vars(argparse.parse_args())
 
+    logging_file_location = arguments["logging_file_location"]
+    rabbitmq_host = arguments["rabbitmq_host"]
+    alerter_security_config_queue_name = arguments["alerter_security_config_queue_name"]
+    exchange_name = arguments["exchange_name"]
+
+    print(f"Received arguments: \n logging_file_location = {logging_file_location} \n"
+          f" rabbitmq_host = {rabbitmq_host} \n"
+          f" alerter_security_config_queue_name = {alerter_security_config_queue_name} \n "
+          f"exchange_name = {exchange_name}")
+
+    logging_setup = LoggingSetup(logging_file_location)
     security_status_alerter_service = SecurityStatusAlerterService()
     security_state_alerter_service = SecurityStateAlerterService()
     alerter_service = AlerterService(security_status_alerter_service,
